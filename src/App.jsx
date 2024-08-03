975b32aec54397f624008f745de0be3bb9f2dfa5
Maïssane Abdallah <abd.maissane@gmail.com>
2024-08-03 11:53:32 +0200

    Mise en place react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import './App.css'
 import Home from './Home'
+import SingleMovie from './SingleMovie';
+import Error from './Error';
+import {Routes, Route} from "react-router-dom";
 
 function App() {
 
   return (
     <>
-    <Home/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="movie/:id" element={<SingleMovie/>}/>
+      <Route path="*" element={<Error/>}/>
+    </Routes>
     </>
   )
 }
--- src/SingleMovie.jsx
+++ src/SingleMovie.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import {useParams} from "react-router-dom"
 
 function SingleMovie() {
+
+  const {id} = useParams();
+
   return (
-    <div>SingleMovie</div>
+    <div>Our Single Movie {id}</div>
   )
 }
 
